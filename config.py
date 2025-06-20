58f7093a61e2785e8f56bd4a92549124051ba633
Genevieve Chermside <genevieve_chermside@brown.edu>
2025-06-20 16:59:23 -0400

    mid process of organizing code

---
+++ config.py
@@ -0,0 +1,18 @@
+# config.py
+
+# Directories
+DATA_DIR = 'data/'
+MODEL_DIR = 'models/'
+
+# Data file paths
+X_TRAIN_PATH = DATA_DIR + 'X_train.pkl'
+Y_TRAIN_PATH = DATA_DIR + 'y_train.pkl'
+X_TEST_PATH = DATA_DIR + 'X_test.pkl'
+Y_TEST_PATH = DATA_DIR + 'y_test.pkl'
+
+# Model paths
+SVC_MODEL_PATH = MODEL_DIR + 'svc_model.pkl'
+RF_MODEL_PATH = MODEL_DIR + 'random_forest_model.pkl'
+
+# Experiment metadata
+SEED = 100

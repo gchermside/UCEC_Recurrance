9c5f4e90e18aa4b034a5b399898e698f29a201dd
Genevieve Chermside <genevieve_chermside@brown.edu>
2025-06-20 17:35:31 -0400

    finished organizing code, still need to fix SVC model

--- config.py
+++ config.py
@@ -11,8 +11,9 @@ X_TEST_PATH = DATA_DIR + 'X_test.pkl'
 Y_TEST_PATH = DATA_DIR + 'y_test.pkl'
 
 # Model paths
-SVC_MODEL_PATH = MODEL_DIR + 'svc_model.pkl'
+SVC_MODEL_PATH = MODEL_DIR + 'SVC_no_LASSO.pkl'
 RF_MODEL_PATH = MODEL_DIR + 'random_forest_model.pkl'
+LASSO_MODEL_PATH = MODEL_DIR + 'lasso_model.pkl'
 
 # Experiment metadata
 SEED = 100

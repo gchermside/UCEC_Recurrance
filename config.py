ddfec77d73ff90cb665957d5dde7513b9fd8d9d1
Genevieve Chermside <genevieve_chermside@brown.edu>
2025-08-26 17:15:07 -0400

    Made a function that finds the top features and their weights/importance that works for both logistic_regression and Random Forest.

--- config.py
+++ config.py
@@ -9,12 +9,13 @@ X_TRAIN_PATH = DATA_DIR + 'X_train.pkl'
 Y_TRAIN_PATH = DATA_DIR + 'y_train.pkl'
 X_TEST_PATH = DATA_DIR + 'X_test.pkl'
 Y_TEST_PATH = DATA_DIR + 'y_test.pkl'
-FEATURE_NAMES = DATA_DIR + "feature_names.pkl"
+FEATURE_NAMES = DATA_DIR + 'feature_names.pkl'
 
 # Model paths
 SVC_MODEL_PATH = MODEL_DIR + 'SVC_no_LASSO.pkl'
 RF_MODEL_PATH = MODEL_DIR + 'random_forest_model.pkl'
 LASSO_MODEL_PATH = MODEL_DIR + 'lasso_model.pkl'
+LR_MODEL_PATH = MODEL_DIR + 'logistic_regression.pkl'
 
 # Experiment metadata
 SEED = 100
